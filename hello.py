be353b79b6183ccacc4cf97acfddc9185499b8ee
jewel <jewelposniak@gmail.com>
2019-05-13 08:25:11 -0400

    basic DB analysis

--- hello.py
+++ hello.py
@@ -1,2 +1,15 @@
-msg = "Hello World"
-print(msg)
+import sqlite3
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import scale
+from customplot import *
+
+# Create your connection.
+cnx = sqlite3.connect('database.sqlite')
+df = pd.read_sql_query("SELECT * FROM Player_Attributes", cnx)
+
+df.columns
+
+print(df.describe().transpose())
